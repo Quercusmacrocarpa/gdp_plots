6b5690cdbd15d43c3defa1b640fecbccb95777b7
Caitlin Carlson <cmcarlson5@wisc.edu>
2018-08-30 14:02:36 -0500

    takes commandline argument from nano into data file

--- gdp_plots.py
+++ gdp_plots.py
@@ -2,18 +2,21 @@ import pandas
 # we need to import part of matplotlib
 # because we are no longer in a notebook
 import matplotlib.pyplot as plt
+import sys
 
 # load data and transpose so that country names are
 # the columns and their gdp data becomes the rows
 
 # read data into a pandas dataframe and transpose
-data = pandas.read_csv('gapminder_gdp_oceania.csv', index_col = 'country').T
+filename=sys.argv[1]
+data = pandas.read_csv(filename, index_col = 'country').T
 
 # create a plot the transposed data
+
 ax = data.plot (title=filename)
 
 # axes labels
-ax.set_xlabel('year)
+ax.set_xlabel('year')
 ax.set_ylabel('GDP Per Capita')
 
 #set axes ticks
